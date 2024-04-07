3e67172b0e6aca45f6fd33e972b711cfc32f9791
gdelgador <67815621+gdelgador@users.noreply.github.com>
2024-04-07 18:28:45 +0000

    x

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+listado_nombres =['Miguel Ángel',
+'Juan Carlos',
+'Carlos Alberto',
+'José Luis',
+'Juan Manuel']
+
+
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def index():
+    return render_template("index.html", title="Listado de Nombres", nombres= listado_nombres)
